6ac1e42832439b6d84f40b6a4dce27511aff2b37
Nikhil Tiwary <ntiwary61@gmail.com>
2023-08-08 20:15:08 +0530

    refactor: replace if-statements with switch-case

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import { createStore, applyMiddleware } from "redux";
 import logger from "redux-logger";
+import axios from "axios";
 
 const history = [];
 const inc = "increment";
@@ -12,17 +13,19 @@ const store = createStore(reducer, applyMiddleware(logger.default));
 //reducer
 function reducer(state = { amount: 1 }, action) {
   //immutability
-  if (action.type === inc) {
-    return { amount: state.amount + 1 };
+  switch (action.type) {
+    case inc:
+      return { amount: state.amount + 1 };
+      break;
+    case dec:
+      return { amount: state.amount - 1 };
+      break;
+    case incByAmt:
+      return { amount: state.amount + action.payload };
+      break;
+    default:
+      return state;
   }
-  if (action.type === dec) {
-    return { amount: state.amount - 1 };
-  }
-  if (action.type === incByAmt) {
-    return { amount: state.amount + action.payload };
-  }
-
-  return state;
 }
 
 //global state
