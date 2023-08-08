23045e3f2bf16299c6df440c9e84e31ecee9f9a1
Nikhil Tiwary <ntiwary61@gmail.com>
2023-08-08 19:33:27 +0530

    add action creators

--- index.js
+++ index.js
@@ -1,26 +1,47 @@
-import { createStore } from "redux";
-
-//store
-const store = createStore(reducer);
+import { createStore, applyMiddleware } from "redux";
+import logger from "redux-logger";
 
 const history = [];
+const inc = "increment";
+const dec = "decrement";
+const incByAmt = "incrementByAmount";
+
+//store
+const store = createStore(reducer, applyMiddleware(logger.default));
 
 //reducer
 function reducer(state = { amount: 1 }, action) {
   //immutability
-  if (action.type === "increment") {
+  if (action.type === inc) {
     return { amount: state.amount + 1 };
+  }
+  if (action.type === dec) {
+    return { amount: state.amount - 1 };
+  }
+  if (action.type === incByAmt) {
+    return { amount: state.amount + action.payload };
   }
 
   return state;
 }
 
 //global state
-store.subscribe(() => {
-  history.push(store.getState());
-  console.log(history);
-});
+// store.subscribe(() => {
+//   history.push(store.getState());
+//   console.log(history);
+// });
+
+//Action creators
+function increment() {
+  return { type: inc };
+}
+function decrement() {
+  return { type: dec };
+}
+function incrementByAmount(amt) {
+  return { type: incByAmt, payload: amt };
+}
 
 setInterval(() => {
-  store.dispatch({ type: "increment" });
+  store.dispatch(incrementByAmount(4));
 }, 2000);
