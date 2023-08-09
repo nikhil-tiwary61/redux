a939d292422d014761e9f67607d336ef1d8a27ea
Nikhil Tiwary <ntiwary61@gmail.com>
2023-08-09 15:07:38 +0530

    feat: add state management for pending and failed cases of API fetching
    proper error handling of API is done using try and catch

--- index.js
+++ index.js
@@ -1,63 +1,82 @@
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, combineReducers } from "redux";
 import logger from "redux-logger";
 import thunk from "redux-thunk";
 import axios from "axios";
 
 const history = [];
-const init = "init";
-const inc = "increment";
-const dec = "decrement";
-const incByAmt = "incrementByAmount";
+// const init = "account/init";
+const inc = "account/increment";
+const dec = "account/decrement";
+const incByAmt = "account/incrementByAmount";
+const getAccUserPending = "account/getUser/pending";
+const getAccUserFulfilled = "account/getUser/fulfilled";
+const getAccUserRejected = "account/getUser/rejected";
+const incBonus = "bonus/increment";
 
 //store
 const store = createStore(
-  reducer,
+  combineReducers({
+    account: accountReducer,
+    bonus: bonusReducer,
+  }),
   applyMiddleware(logger.default, thunk.default)
 );
 
 //reducer
-function reducer(state = { amount: 1 }, action) {
+function accountReducer(state = { amount: 1 }, action) {
   //immutability
   switch (action.type) {
-    case init:
-      return { amount: action.payload };
-      break;
+    case getAccUserFulfilled:
+      return { amount: action.payload, pending: false };
+    case getAccUserRejected:
+      return { ...state, error: action.error, pending: false };
+    case getAccUserPending:
+      return { ...state, pending: true };
     case inc:
       return { amount: state.amount + 1 };
-      break;
     case dec:
       return { amount: state.amount - 1 };
-      break;
     case incByAmt:
       return { amount: state.amount + action.payload };
+    default:
+      return state;
+  }
+}
+
+function bonusReducer(state = { points: 0 }, action) {
+  switch (action.type) {
+    case incBonus:
+      return { points: state.points + 1 };
+    case incByAmt:
+      if (action.payload >= 100) {
+        return { points: state.points + 1 };
+      }
       break;
     default:
       return state;
   }
 }
 
-//global state
-// store.subscribe(() => {
-//   history.push(store.getState());
-//   console.log(history);
-// });
-
-//Async API call
-// async function getUser() {
-//   const { data } = await axios.get("http://localhost:3000/accounts/1");
-//   console.log(data);
-// }
-// getUser();
-
 //Action creators
-function getUser(id) {
+function getUserAccount(id) {
   return async (dispatch, getUser) => {
-    const { data } = await axios.get(`http://localhost:3000/accounts/${id}`);
-    dispatch(initUser(data.amount));
+    try {
+      dispatch(getAccountUserPending());
+      const { data } = await axios.get(`http://localhost:3000/accounts/${id}`);
+      dispatch(getAccountUserFulfilled(data.amount));
+    } catch (error) {
+      dispatch(getAccountUserRejected(error.message));
+    }
   };
 }
-function initUser(value) {
-  return { type: init, payload: value };
+function getAccountUserFulfilled(value) {
+  return { type: getAccUserFulfilled, payload: value };
+}
+function getAccountUserRejected(error) {
+  return { type: getAccUserRejected, error: error };
+}
+function getAccountUserPending() {
+  return { type: getAccUserPending };
 }
 function increment() {
   return { type: inc };
@@ -68,7 +87,11 @@ function decrement() {
 function incrementByAmount(amt) {
   return { type: incByAmt, payload: amt };
 }
+function incrementBonus() {
+  return { type: incBonus };
+}
 
 setTimeout(() => {
-  store.dispatch(getUser(2));
+  store.dispatch(getUserAccount(2));
+  //   store.dispatch(incrementBonus());
 }, 2000);
